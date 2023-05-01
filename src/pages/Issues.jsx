5143896d642289b72d6e795239dd8fd8a3404106
akibhasan50 <tonuakibhasan@gmail.com>
2023-05-01 14:38:26 +0600

    added labels with select and deselect functionality

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 export default function Issues() {
+  const [labels, setLabels] = useState([]);
+  console.log(labels);
   return (
     <div>
       <main>
@@ -9,7 +12,18 @@ export default function Issues() {
           <IssuesList />
         </section>
         <aside>
-          <LabelList />
+          <LabelList
+            selected={labels}
+            toggle={(label) =>
+              setLabels((currentLabels) => {
+                return currentLabels?.includes(label)
+                  ? currentLabels?.filter(
+                      (currentLabel) => currentLabel !== label
+                    )
+                  : currentLabels?.concat(label);
+              })
+            }
+          />
         </aside>
       </main>
     </div>
