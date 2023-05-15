936aab47754a23ce39ad6fc3cda8b7232196319e
akibhasan50 <tonuakibhasan@gmail.com>
2023-05-15 23:38:03 +0600

    added placeholder data in labels

--- src/helpers/useLabelsData.js
+++ src/helpers/useLabelsData.js
@@ -1,10 +1,11 @@
 import { useQuery } from "react-query";
-
+import {defaultLabels} from './defaultData';
 export const useLabelsData = () => {
   const labelsQuery = useQuery(["labels"], ({signal}) => {
     return fetch("/api/labels",{signal}).then((res) => res.json());
   },  {
     staleTime: 1000 * 60 * 60,
+    placeholderData:defaultLabels
   });
   return labelsQuery;
 };
